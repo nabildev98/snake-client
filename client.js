20975d5162a103bbff90ebce57c1798b5eaab13f
vagrant <nabilahmed98@hotmail.com>
2022-09-01 01:07:46 +0000

    first commit

---
+++ client.js
@@ -0,0 +1,16 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541,
+  });
+  conn.setEncoding("utf8");
+  conn.on("data", (data) => {
+    console.log(data);
+  });
+  return conn;
+};
+
+module.exports = connect;
