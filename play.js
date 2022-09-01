0b1e7dfa75f100cd108a9d09ba9e7c5f6faa5c80
vagrant <nabilahmed98@hotmail.com>
2022-09-01 23:23:06 +0000

    snakeee

--- play.js
+++ play.js
@@ -1,19 +1,6 @@
 const connect = require("./client");
 const setupInput = require("./input");
-// // establishes a connection with the game server
-// const connect = function () {
-//   const conn = net.createConnection({
-//     host: "localhost",
-//     port: 50541,
-//   });
 
-// interpret incoming data as text
-
-// };
-// setup interface to handle user input from stdin
-
-// setupInput();
 console.log("Connecting ...");
-connect();
 
-setupInput();
+setupInput(connect());
